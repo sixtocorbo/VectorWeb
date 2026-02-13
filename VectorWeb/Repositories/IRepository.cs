775da27dd26df775251741e32fef2fbfeb808c41
Sixto Corbo <53678874+sixtocorbo@users.noreply.github.com>
2026-02-13 03:11:45 -0300

    Add catalog maintenance pages and repository includes support

--- VectorWeb/Repositories/IRepository.cs
+++ VectorWeb/Repositories/IRepository.cs
@@ -4,6 +4,7 @@ namespace VectorWeb.Repositories
 {
     public interface IRepository<T> where T : class
     {
+        IQueryable<T> GetQueryable(string includeProperties = "");
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
