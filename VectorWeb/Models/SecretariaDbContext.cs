104c2e0d035637b1d9ff72cb22ab161fb5a5b320
Sixto Corbo <53678874+sixtocorbo@users.noreply.github.com>
2026-02-14 15:58:30 -0300

    Corrige advertencias de compilación CS1030 y CS4014

--- VectorWeb/Models/SecretariaDbContext.cs
+++ VectorWeb/Models/SecretariaDbContext.cs
@@ -56,8 +56,18 @@ public partial class SecretariaDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=SecretariaDB;Trusted_Connection=True;TrustServerCertificate=True");
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
