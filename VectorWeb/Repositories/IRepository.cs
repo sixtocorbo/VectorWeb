ca3638e6627a35149673a188835f824006c86a09
Sixto Corbo <53678874+sixtocorbo@users.noreply.github.com>
2026-02-14 05:29:54 -0300

    Fix nullable warnings reported in rebuild output

--- VectorWeb/Repositories/IRepository.cs
+++ VectorWeb/Repositories/IRepository.cs
@@ -6,7 +6,7 @@ namespace VectorWeb.Repositories
     {
         IQueryable<T> GetQueryable(string includeProperties = "");
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
+        Task<T?> GetByIdAsync(int id);
         Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
         Task AddAsync(T entity);
         Task UpdateAsync(T entity);
