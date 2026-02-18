dfed879d4bbc0fd96f89945a8b7d44b450ce0c30
Melanie <sixtocorbobarreto@gmail.com>
2026-02-17 23:22:45 -0300

    Updated

--- VectorWeb/Models/SecretariaDbContext.cs
+++ VectorWeb/Models/SecretariaDbContext.cs
@@ -58,7 +58,8 @@ public partial class SecretariaDbContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LOCALHOST;Database=SecretariaDB;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
